d312306abed99ad2d410767b87ef3f4340372f80
Giovanni <giovannipor23@gmail.com>
2021-03-22 16:50:38 +0100

    HTML/CSS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 var app = new Vue ({
     el: "#root",
     data: {
-        message: ""
-    }
+        message: "",
+        items: [
+            "Buy milk",
+            "Clean the car",
+            "Do exercise"
+        ]
+    },
+/*     methods: {
+
+        addItem(item) {
+            this.items.push(item);
+        },
+
+        rmvItem(index) {
+            this.items.splice(index, 1);
+        }
+    } */
 });
+
+console.log(app.data);
